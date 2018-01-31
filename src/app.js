bee1887f31a616354f0acef6d0c33d13330e17bd
Richard Sim <rsim@devras.com>
2018-01-31 11:25:43 -0800

    Removing dead code

--- src/app.js
+++ src/app.js
@@ -12,8 +12,6 @@ import { remote } from "electron";
 import jetpack from "fs-jetpack";
 import { greet } from "./hello_world/hello_world";
 import env from "env";
-// let HID = require('node-hid');
-// let btSerial = new (require('bluetooth-serial-port')).BluetoothSerialPort();
 let bwipjs = require('bwip-js');
 
 const app = remote.app;
@@ -107,100 +105,3 @@ window.addEventListener("keydown", function(event) {
   // Consume the event so it doesn't get handled twice
   event.preventDefault();
 }, true);
-
-
-// let devices = HID.devices();
-// console.log(devices, null, 4);
-
-/*let deviceInfo = devices.find(function(d) {
-  return (d.vendorId == 13) && (d.productId == 0);
-});
-if (deviceInfo) {
-  let barcodeScanner = new HID.HID(deviceInfo.path);
-  let barcodeData = [];
-
-  barcodeScanner.on("data", function(data) {
-    barcodeData.push(data);
-    console.log(JSON.stringify(data, null, 4));
-
-    document.querySelector("#hid-data").innerHTML = JSON.stringify(barcodeData, null, 4);
-
-    if (data[2] == 40) {
-      barcodeData = [];
-    }
-  });
-}*/
-
-
-// btSerial.on('found', function(address, name) {
-//     console.log('found device at ' + address + ' (' + name + ')');
-//     btSerial.findSerialPortChannel(address, function(channel) {
-//         console.log('' + address + ' has serial port on channel ' + channel);
-//         btSerial.connect(address, channel, function() {
-//             console.log('connected to ' + address + ' on channel ' + channel);
-//
-//             let btData = [];
-//             btSerial.on('data', function(buffer) {
-//                 btData.push(...buffer);
-//                 console.log(JSON.stringify(buffer, null, 4));
-//
-//                 let bufferArr = [...buffer];
-//                 for (let ch of bufferArr) {
-//                   if (ch == 0x13) {
-//                     console.log(JSON.stringify(btData, null, 4));
-//                     console.log('disconnecting');
-//                     // close the connection when you're ready
-//                     btSerial.close();
-//
-//                     document.querySelector("#bt-data").innerHTML = JSON.stringify(btData, null, 4);
-//                     btData = [];
-//
-//                     break;
-//                   }
-//                 }
-//            });
-//         }, function () {
-//             console.log('cannot connect to ' + address + ' on channel ' + channel);
-//         });
-//      }, function() {
-//         console.log('no serial port channel found for ' + address);
-//     });
-// });
-// btSerial.on('failure', function(err) {
-//   console.log('BT error: ' + JSON.stringify(err, null, 4));
-// });
-// btSerial.on('closed', function() {
-//   console.log('connection closed.');
-// });
-// btSerial.on('finished', function() {
-//   console.log('search finished.');
-//
-//   btSerial.listPairedDevices(function(devices) {
-//     console.log(`${devices.length} BlueTooth device(s) are connected.`);
-//     for (let dev of devices) {
-// //         console.log('Paired with: ' + JSON.stringify(dev, null, 4));
-//
-//         if (dev.name.indexOf('SL_') === 0) {
-//             console.log('Trying to connect to: ' + JSON.stringify(dev, null, 4));
-//             let address = dev.address;
-//             btSerial.findSerialPortChannel(address, function(channel) {
-//                 console.log('' + address + ' has serial port on channel ' + channel);
-//                 btSerial.connect(address, channel, function() {
-//                     console.log('connected to ' + address + ' on channel ' + channel);
-//                     console.log(`CONNECTED >>> `);
-//                     btSerial.close();
-//                 }, function () {
-//                     console.log('cannot connect to ' + address + ' on channel ' + channel);
-//                 });
-//              }, function() {
-//                 console.log('no serial port channel found for ' + address);
-//             });
-//         }
-//     };
-//   });
-// });
-//
-// btSerial.inquire();
-// console.log('searching for BT serial port devices...');
-
-
