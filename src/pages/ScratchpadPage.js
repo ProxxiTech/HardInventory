15e5a65d0b93346bf70d0c9aa014e5ff218e1d39
Richard Sim <rsim@devras.com>
2018-02-19 01:39:49 -0800

    Scratchpad code to fix up raw 1D barcodes in the spreadsheet

--- src/pages/ScratchpadPage.js
+++ src/pages/ScratchpadPage.js
@@ -1,4 +1,5 @@
-// import * as spreadsheet from "../spreadsheet/spreadsheet";
+import * as spreadsheet from "../spreadsheet/spreadsheet";
+import barcodeScanner from "../helpers/barcodeScanner";
 
 import AppPage from "./AppPage";
 
@@ -8,176 +9,104 @@ class ScratchpadPage extends AppPage {
     super(data);
   }
 
-  isInternalPartNumber(data) {
-    let prefixPN = "IPN-";
-
-    if (data.startsWith(prefixPN)) {
-      let internalPartNumber = data.substring(prefixPN.length);
-      let pnPrefixEnd = internalPartNumber.indexOf("-");
-      if (pnPrefixEnd > 0) {
-        let pnPrefix = internalPartNumber.substring(0, pnPrefixEnd);
-        let pnSuffix = internalPartNumber.substring(pnPrefixEnd + 1);
-        if ((pnPrefix.length > 0) && (pnSuffix.length > 0)) {
-          // TODO: Validate that pnPrefix is a valid/existing Category
-
-          // Valid PN's are alphanumeric only
-          if (pnSuffix.indexOf("-") < 0) {
-            return true;
-          }
-        }
-      }
-    }
-
-    return false;
-  }
-
-  isLocationID(data) {
-    let prefixLocID = "LOC-";
-
-    if (data.startsWith(prefixLocID)) {
-      let locID = data.substring(prefixLocID.length);
-      if (locID.length > 0) {
-        return true;
-      }
-    }
-
-    return false;
-  }
-
-  findDataMatrixBlock(kbeData, blockPrefix) {
-    for (let block of kbeData) {
-      let idx = block.indexOf(blockPrefix);
-      if (idx == 0) {
-        return block.substring(idx + blockPrefix.length);
-      }
-    }
-
-    return null;
-  }
-
   onBtnDataMatrix() {
-    let kbeData = [
-      "[)>*F9*06",
-      "P14-0025",
-      "1PGRM033R71E221KA01D",
-      "K1K51687419",
-      "10K5917815111K14L",
-      "Q100",
-      "11ZPICK",
-      "12Z702717",
-      "13Z9046122",
-      "0Z0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000",
-    ];
-
-    this.Elements.category.value = null;
-    this.Elements.pn.value = null;
-    this.Elements.mpn.value = null;
-    this.Elements.qty.value = null;
-
-    // P<internalPartNumber>
-    let internalPartNumber = this.findDataMatrixBlock(kbeData, "P");
-    this.Elements.category.value = null;
-    this.Elements.pn.value = null;
-    if (internalPartNumber) {
-      let pnPrefixEnd = internalPartNumber.indexOf("-");
-      if (pnPrefixEnd > 0) {
-        let pnPrefix = internalPartNumber.substring(0, pnPrefixEnd);
-        let pnSuffix = internalPartNumber.substring(pnPrefixEnd + 1);
-        if ((pnPrefix.length > 0) && (pnSuffix.length > 0)) {
-          // Valid PN's are alphanumeric only
-          if (pnSuffix.indexOf("-") < 0) {
-            this.Elements.category.value = pnPrefix;
-            this.Elements.pn.value = pnSuffix;
-          }
-        }
-      }
-    }
-
-    // 1P<manufacturerPartNumber>
-    let manufacturerPartNumber = this.findDataMatrixBlock(kbeData, "1P");
-    this.Elements.mpn.value = manufacturerPartNumber;
-
-    // Q<quantity>
-    let quantityStr = this.findDataMatrixBlock(kbeData, "Q");
-    let quantity = quantityStr ? parseInt(quantityStr, 10) : 0;
-    this.Elements.qty.value = quantity.toString();
   }
 
   onBtnPDF417() {
-    let kbeData = [
-      "[)>*F9*06",
-      "J28A55EBB.F0264EE8.AF946017.7FFAB16D30C9",
-      "PNSR01L30NXT5GOSDKR-ND",
-      "1PNSR01L30NXT5G",
-      "9D1522",
-      "14Z150524",
-      "1TDL167851A",
-      "4LCN",
-      "11ZPREPACK",
-      "15Z1.0.1",
-      "12Z3488042",
-      "Q000000200",
-      "13Z000000",
-    ];
+    // if (data.startsWith("[)>06"))
+  }
 
-    this.Elements.category.value = null;
-    this.Elements.pn.value = null;
-    this.Elements.mpn.value = null;
-    this.Elements.qty.value = null;
-
-    // P<internalPartNumber>
-    let internalPartNumber = this.findDataMatrixBlock(kbeData, "P");
-    this.Elements.category.value = null;
-    this.Elements.pn.value = null;
-    if (internalPartNumber) {
-      let pnPrefixEnd = internalPartNumber.indexOf("-");
-      if (pnPrefixEnd > 0) {
-        let pnPrefix = internalPartNumber.substring(0, pnPrefixEnd);
-        let pnSuffix = internalPartNumber.substring(pnPrefixEnd + 1);
-        if ((pnPrefix.length > 0) && (pnSuffix.length > 0)) {
-          // Valid PN's are alphanumeric only
-          if (pnSuffix.indexOf("-") < 0) {
-            this.Elements.category.value = pnPrefix;
-            this.Elements.pn.value = pnSuffix;
-          }
-        }
-      }
+  updateNextResult(results) {
+    if (!results || !results.length) {
+      console.log("Completed 1D barcode update!");
+      return;
     }
 
-    // 1P<manufacturerPartNumber>
-    let manufacturerPartNumber = this.findDataMatrixBlock(kbeData, "1P");
-    this.Elements.mpn.value = manufacturerPartNumber;
+    let result = results.shift();
+    let {rowIdx, item} = result;
 
-    // Q<quantity>
-    let quantityStr = this.findDataMatrixBlock(kbeData, "Q");
-    let quantity = quantityStr ? parseInt(quantityStr, 10) : 0;
-    this.Elements.qty.value = quantity.toString();
+    //
+    let data = item["Manufacturer Part Number"];
+    if ((data.length == 22) && (/^\d+$/.test(data))) {
+      let kbeData = [ data ];
+      barcodeScanner.processSimpleBarcode(kbeData, (results) => {
+        let {
+          internalPN,
+          // isNewInternalPN,
+          // supplierPN,
+
+          locationID,
+
+          manufacturerPN,
+          manufacturer,
+          // category,
+          description,
+
+          quantity
+        } = results;
+
+        if (manufacturerPN) {
+          let existingResult = spreadsheet.findInventoryItemByMPN(manufacturerPN);
+          if (existingResult) {
+            let existingRowIdx = existingResult.rowIdx;
+            let existingItem = existingResult.item;
+
+            let newLocID = item["Location"] || locationID;
+            let oldLocID = existingItem["Location"];
+            if (!existingItem["Location"]) {
+              existingItem["Location"] = newLocID;
+            }
+            existingItem["Part Number"] = `="${internalPN}"`;
+            existingItem["Manufacturer Part Number"] = manufacturerPN;
+            existingItem["Manufacturer"] = manufacturer;
+            existingItem["Description"] = description;
+            existingItem["Quantity"] = parseInt(existingItem["Quantity"], 10) + quantity;
+
+            manufacturer = '=""';
+            description = '=""';
+            quantity = 0;
+
+            if (oldLocID && (oldLocID !== newLocID)) {
+              description = `*** MOVE TO ${existingItem["Location"]} [existing parts are there]`;
+            }
+
+            spreadsheet.setInventoryItem(existingRowIdx, existingItem, (err) => {
+              if (err) {
+                return console.error(err);
+              }
+            });
+          }
+        }
+
+        if (!item["Location"]) {
+          item["Location"] = locationID;
+        }
+        item["Part Number"] = `="${internalPN}"`;
+        item["Manufacturer Part Number"] = manufacturerPN;
+        item["Manufacturer"] = manufacturer;
+        item["Quantity"] = quantity;
+        item["Description"] = description;
+
+        spreadsheet.setInventoryItem(rowIdx, item, (err) => {
+          if (err) {
+            return console.error(err);
+          }
+        });
+      });
+    } else {
+      return this.updateNextResult(results);
+    }
+
+    //
+    setTimeout(() => {
+      this.updateNextResult(results);
+    }, 1000);
   }
 
   onBtn1D() {
-    let kbeData = [
-      "5195612000000010880707"
-    ];
-
-    this.Elements.category.value = null;
-    this.Elements.pn.value = null;
-    this.Elements.mpn.value = null;
-    this.Elements.qty.value = null;
-
-    let data = kbeData[0];
-    if ((data.length == 22) && (/^\d+$/.test(data))) {
-      let dkPartID = data.substring(0, 7);
-      let orderQty = parseInt(data.substring(7, 16), 10);
-
-      this.Elements.qty.value = orderQty;
-
-
-      // TODO
-      this.Elements.category.value = "0";
-      this.Elements.pn.value = `${dkPartID}`;
-
-      this.Elements.mpn.value = null;
-    }
+    spreadsheet.findInventoryItemsByCategory("0", (results) => {
+      this.updateNextResult(results);
+    });
   }
 
   onInitialize(appState) {
